6a0bf1178906cd29a8b4a80e8f873e0aef23feeb
Archana M <archanamani1995@gmail.com>
2025-02-19 15:32:25 +0530

    changes for packages

--- test/obj/Debug/net8.0/test.AssemblyInfo.cs
+++ test/obj/Debug/net8.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cd55d0b278c3e1688b0428020d5162072d876f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4d2e471445c9bd042298d15d668e684715e661c")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
